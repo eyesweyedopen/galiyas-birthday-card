b28fa251b951368b3948124ef5002db083593793
Baruch Elias <eyesweyedopen@gmail.com>
2018-11-29 00:22:09 -0500

    Updates

---
+++ precache-manifest.7d1e625991834b53501e828d205ec195.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/galiyas-birthday-card/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c8bd23db99541facdbda",
+    "url": "/galiyas-birthday-card/static/js/runtime~main.c8bd23db.js"
+  },
+  {
+    "revision": "8ae4ede628c9f7bcd470",
+    "url": "/galiyas-birthday-card/static/js/main.8ae4ede6.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/galiyas-birthday-card/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "8ae4ede628c9f7bcd470",
+    "url": "/galiyas-birthday-card/static/css/main.2599d695.chunk.css"
+  },
+  {
+    "revision": "bc53ac95e4703a1664f45fc8e041e8db",
+    "url": "/galiyas-birthday-card/index.html"
+  }
+];
